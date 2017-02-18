42ecb888d709465c5e23e71d24269ebb33910313
sxf <sunxfancy@gmail.com>
2017-02-18 17:18:03 +0800

    fix most error change lexer and parser become reentrant
    Signed-off-by: sxf <sunxfancy@gmail.com>

---
+++ core/src/fir.h
@@ -0,0 +1,19 @@
+//
+// Created by sxf on 17-2-18.
+//
+
+#ifndef FIR_FIR_H
+#define FIR_FIR_H
+
+#include "Model/Node.h"
+
+extern "C" {
+
+extern Node* parseFile(const char* path);
+
+
+
+
+};
+
+#endif //FIR_FIR_H
